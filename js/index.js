36a72c1a02142e8dc4ca9ba55c8a957bd24a7433
ahazard <jane15q@aol.com>
2022-08-06 19:37:14 -0400

    add images, add classes to boxes, minor visual tweaks

---
+++ js/index.js
@@ -0,0 +1,22 @@
+// do i add card images here?
+let cardImages = {
+    blueGhost: "https://i.imgur.com/nEofhgx.png", 
+    redGhost: "https://i.imgur.com/0D0tsvT.png", 
+    pacman: "https://i.imgur.com/O1Prg8x.png",
+    cherries: "https://i.imgur.com/3XRSM1A.png",
+    orange: "https://i.imgur.com/lDO7yXf.png"
+}
+
+let box = document.getElementsByClassName("box");
+
+// add event listener for box
+// box.addEventListener("click", function for turning over a card);
+
+// add function for turnLogic
+
+// for timer
+// if (click === -1) {
+//     timer = setInterval(function() {
+//       time++;
+//       timeElt.innerHTML = time;
+//     }, 1000);
