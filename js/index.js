2baa914a5ace4d51a3e57d4ed6f634bde6038547
ahazard <jane15q@aol.com>
2022-08-09 16:33:58 -0400

    images show in each box, are randomized upon refresh

--- js/index.js
+++ js/index.js
@@ -1,19 +1,39 @@
-// let cardImages = {
-//     blueGhost: "../images/pacman-blueghost", 
-//     sue: "../images/pacman-sue.jpeg", 
-//     pacman: "../images/pacman-pacman.jpeg",
-//     cherries: "../images/pacman-cherries.jpeg",
-//     inky: "../images/pacman-inky.jpeg",
-//     clyde: "../images/pacman-clyde.jpeg",
-//     pinky: "../images/pacman-pinky.jpeg",
-//     strawberry: "../images/pacman-strawberry.jpg"
-// };
+let cardImages = [
+    "../images/pacman-blueghost.jpeg",
+    "../images/pacman-sue.jpeg",
+    "../images/pacman.jpeg",
+    "../images/pacman-cherries.jpeg",
+    "../images/pacman-inky.jpeg",
+    "../images/pacman-clyde.jpeg",
+    "../images/pacman-pinky.jpeg",
+    "../images/pacman-strawberry.jpg",
+    "../images/pacman-blueghost.jpeg",
+    "../images/pacman-sue.jpeg",
+    "../images/pacman.jpeg",
+    "../images/pacman-cherries.jpeg",
+    "../images/pacman-inky.jpeg",
+    "../images/pacman-clyde.jpeg",
+    "../images/pacman-pinky.jpeg",
+    "../images/pacman-strawberry.jpg"
+];
 
-let box = document.getElementById("box1");
-    //this is an array of all the box divs
+
+
+
+    //returns an array of all boxes with class "box"
 let score = 0;
 let timer = 0;
 let start = document.getElementById("deal");
+
+let boxes = document.getElementsByClassName("box");
+for (let i = 0; i < boxes.length; i++) {
+    let rand = Math.floor(Math.random() * (cardImages.length - 1));
+            //need to correct to only have two of each image
+            boxes[i].innerHTML = "<img src='" + cardImages[rand] + "' alt='image' class='hidden' width=100% height=100%>"
+            boxes[i].style.width = "8rem"
+            boxes[i].style.height = "8rem"
+        cardImages.splice(rand, 1)
+}
 
 
 // for timer (on first click)
@@ -29,10 +49,26 @@ let start = document.getElementById("deal");
     //randomize cards
         //math.floor(math.random() * 17); - returns a random integer from 0 to 16
     //// Shuffle array
-        // const shuffled = array.sort(() => 0.5 - Math.random());
+    //Fisher-Yates shuffle
+    // function shuffle(cardImages) {
+    //     let currentIndex = cardImages.length, randomIndex;
+      
+    //     // While there remain elements to shuffle.
+    //     while (currentIndex !== 0) {
+      
+    //       // Pick a remaining element.
+    //       randomIndex = Math.floor(Math.random() * currentIndex);
+    //       currentIndex--;
+      
+    //       // And swap it with the current element.
+    //       [cardImages[currentIndex], cardImages[randomIndex]] = [
+    //         cardImages[randomIndex], cardImages[currentIndex]];
+    //     }};
+      
+    //     start.addEventListener("click", function() {
+    //         shuffle(cardImages);
+    //   });
 
-    // // Get sub-array of first n elements after shuffled
-    // let selected = shuffled.slice(0, n);
     
     //all cards face down
     //set score to zero
@@ -40,51 +76,21 @@ let start = document.getElementById("deal");
 
 //handle turn
 
-    // add event listener for box to change image
-
-    function image() {
-        box.style.backgroundImage = "url('../images/pacman-cherries.jpeg";;
-    };
-    box.addEventListener("click", function() {
-        image();
-    });
-
-    // function changeImage() {
-    //     document.getElementsByClassName("box").changeImage = cardImages.cherries;
-    //  }; 
-    // box[0].addEventListener("click", function() {
-    //     changeImage();
-    // });
-   
-    // box[0].addEventListener("click", function () {
-    //     alert("Hello world");
+    // add event listener for EACH box to change image
+    // if images are random, how do i show the image that's on the box?
+ 
+ 
+    // function image() {
+    //     boxes.style.backgroundImage = "url('../images/pacman-cherries.jpeg')"
+    // };
+    // boxes.addEventListener("click", function() {
     //     image();
     // });
-    // When you called the image() function, what does it mean?
-    
-    // function image() {
-    // box.style = cardImages.cherries;
-    // };
-    
-    // I think you need to debug this line box.style = cardImages.cherries; 
-    // image.addEventListener('click', function(){
-    //     changeImage();
-    // });
 
-    // function changeImage(){
-    //     image.src = './squid-dab2.gif';
-
-        // consider (mdn): 
-        // function hide(evt) {
-        //     // evt.target refers to the clicked <li> element
-        //     // This is different than evt.currentTarget, which would refer to the parent <ul> in this context
-        //     evt.target.style.visibility = 'hidden';
-        //   }
-          
 
     // add function for turnLogic (check for match)
     //
-    //add funtion to increase score
+    //add function to increase score
 
 
 //score
@@ -92,3 +98,89 @@ let start = document.getElementById("deal");
 
 
 //show win box
+
+// let cardImages = [
+//     "js/pexels-moldy-vintages-8104324.jpg",
+//     "js/pexels-alleksana-4156998.jpg",
+//     "js/pexels-alvaro-arano-12977998.jpg",
+//     "js/pexels-andrew-neel-13145853.jpg",
+//     "js/pexels-cottonbro-10669673.jpg",
+//     "js/pexels-cup-of-couple-8473212.jpg",
+//     "js/pexels-ekrulila-13025682.jpg",
+//     "js/pexels-hilal-bilir-12861655.jpg",
+//      "js/pexels-moldy-vintages-8104324.jpg",
+//     "js/pexels-alleksana-4156998.jpg",
+//     "js/pexels-alvaro-arano-12977998.jpg",
+//     "js/pexels-andrew-neel-13145853.jpg",
+//     "js/pexels-cottonbro-10669673.jpg",
+//     "js/pexels-cup-of-couple-8473212.jpg",
+//     "js/pexels-ekrulila-13025682.jpg",
+//     "js/pexels-hilal-bilir-12861655.jpg"
+      
+// ];
+// let box = document.getElementsByClassName("box");
+
+
+// for (let i = 0; i < box.length; i++) {
+//     let rand = Math.floor(Math.random() * (cardImages.length - 1));
+//     box[i].innerHTML = "<img src='" + cardImages[rand] + "' alt='image' class='hidden' width=100% height=100%>"
+//     box[i].style.width = "8rem"
+//     box[i].style.height = "8rem"
+// }cardImages.splice(rand, 1);
+//         //this is an array of all the box divs
+//     let score = 0;
+//     let timer = 0;
+    // for timer (on first click)
+    // if (click === -1) {
+    //     timer = setInterval(function() {
+    //       time++;
+    //       timeElt.innerHTML = time;
+    //     }, 1000);
+    //initialize board
+        //randomize cards
+            //math.floor(math.random() * 17); - returns a random integer from 0 to 16
+        //// Shuffle array
+            // const shuffled = array.sort(() => 0.5 - Math.random());
+        // // Get sub-array of first n elements after shuffled
+        // let selected = shuffled.slice(0, n);
+        //all cards face down
+        //set score to zero
+        //set time to zero
+    //handle turn
+        // add event listener for box to change image
+        // function image() {
+        //         box.style.backgroundImage = "url('js/pexels-moldy-vintages-8104324.jpg')"
+        //      };
+        // box.addEventListener("click", function() {
+        //     image();
+        // });
+        // function changeImage() {
+        //     document.getElementsByClassName("box").changeImage = cardImages.cherries;
+        //  };
+        // box[0].addEventListener("click", function() {
+        //     changeImage();
+        // });
+        // box[0].addEventListener("click", function () {
+        //     alert("Hello world");
+        //     image();
+        // });
+        // When you called the image() function, what does it mean?
+        // function image() {
+        // box.style = cardImages.cherries;
+        // };
+        // I think you need to debug this line box.style = cardImages.cherries;
+        // image.addEventListener('click', function(){
+        //     changeImage();
+        // });
+        // function changeImage(){
+        //     image.src = './squid-dab2.gif';
+            // consider (mdn):
+            // function hide(evt) {
+            //     // evt.target refers to the clicked <li> element
+            //     // This is different than evt.currentTarget, which would refer to the parent <ul> in this context
+            //     evt.target.style.visibility = 'hidden';
+            //   }
+        // add function for turnLogic (check for match)
+        //
+        //add funtion to increase score
+    //score
