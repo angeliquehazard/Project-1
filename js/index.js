5a663322ffe48a002690e4dd97c364efedf53a24
ahazard <jane15q@aol.com>
2022-08-08 22:34:35 -0400

    fixed images

--- js/index.js
+++ js/index.js
@@ -1,26 +1,29 @@
-let cardImages = {
-    blueGhost: "https://i.imgur.com/nEofhgx.png", 
-    sue: "https://i.imgur.com/0D0tsvT.png", 
-    pacman: "https://i.imgur.com/O1Prg8x.png",
-    cherries: "https://i.imgur.com/3XRSM1A.png",
-    orange: "https://i.imgur.com/lDO7yXf.png",
-    clyde: "https://i.imgur.com/G2QCU6j.png",
-    pinky: "https://i.imgur.com/7Ylz5eT.png",
-    strawberry: "https://i.imgur.com/5QswQ6r.png"
-};
+// let cardImages = {
+//     blueGhost: "../images/pacman-blueghost", 
+//     sue: "../images/pacman-sue.jpeg", 
+//     pacman: "../images/pacman-pacman.jpeg",
+//     cherries: "../images/pacman-cherries.jpeg",
+//     inky: "../images/pacman-inky.jpeg",
+//     clyde: "../images/pacman-clyde.jpeg",
+//     pinky: "../images/pacman-pinky.jpeg",
+//     strawberry: "../images/pacman-strawberry.jpg"
+// };
 
-let box = document.getElementsByClassName("box");
+let box = document.getElementById("box1");
     //this is an array of all the box divs
 let score = 0;
 let timer = 0;
+let start = document.getElementById("deal");
 
 
 // for timer (on first click)
 // if (click === -1) {
+// start.addEventListener("click", setInterval) {
 //     timer = setInterval(function() {
 //       time++;
 //       timeElt.innerHTML = time;
 //     }, 1000);
+// };
 
 //initialize board
     //randomize cards
@@ -37,15 +40,33 @@ let timer = 0;
 
 //handle turn
 
-    // add event listener for box
-    
+    // add event listener for box to change image
+
     function image() {
-            box.style = cardImages.cherries;
-         }; 
-    box[0].addEventListener("click", function() {
+        box.style.backgroundImage = "url('../images/pacman-cherries.jpeg";;
+    };
+    box.addEventListener("click", function() {
         image();
-    }); 
-      
+    });
+
+    // function changeImage() {
+    //     document.getElementsByClassName("box").changeImage = cardImages.cherries;
+    //  }; 
+    // box[0].addEventListener("click", function() {
+    //     changeImage();
+    // });
+   
+    // box[0].addEventListener("click", function () {
+    //     alert("Hello world");
+    //     image();
+    // });
+    // When you called the image() function, what does it mean?
+    
+    // function image() {
+    // box.style = cardImages.cherries;
+    // };
+    
+    // I think you need to debug this line box.style = cardImages.cherries; 
     // image.addEventListener('click', function(){
     //     changeImage();
     // });
@@ -62,6 +83,7 @@ let timer = 0;
           
 
     // add function for turnLogic (check for match)
+    //
     //add funtion to increase score
 
 
